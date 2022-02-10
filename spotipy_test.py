f6ca5e80d8fe7467b7811057e42fa3b4e8b6263f
Abney <abneyjw@appstate.edu>
2022-02-10 15:44:40 -0500

    update!

--- spotipy_test.py
+++ spotipy_test.py
@@ -1,19 +1,6 @@
 import spotipy
 import sys
-import os
-from dotenv import load_dotenv
-
-from pathlib import Path
-
-dotenv_path = Path('path/to/.env')
-
-load_dotenv(dotenv_path=dotenv_path)
-
-SPOTIFY_CLIENT_ID=os.getenv('SPOTIPY_CLIENT_ID')
-
-
-
-print(SPOTIFY_CLIENT_ID)
+import webbrowser
 from spotipy.oauth2 import SpotifyClientCredentials
 
 spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
@@ -21,10 +8,12 @@ spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
 if len(sys.argv) > 1:
     name = ' '.join(sys.argv[1:])
 else:
-    name = 'Radiohead'
+    name = 'STEELY DAN'
 
 results = spotify.search(q='artist:' + name, type='artist')
 items = results['artists']['items']
 if len(items) > 0:
     artist = items[0]
     print(artist['name'], artist['images'][0]['url'])
+
+webbrowser.open(artist['images'][0]['url'])
