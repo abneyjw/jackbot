3d4e0ac6631c8b98c1190f9d7939da5765956dc8
jwabney <jwabney@gmail.com>
2022-02-17 15:07:32 -0500

    more robust user test

--- user_test.py
+++ user_test.py
@@ -1,11 +1,37 @@
 from __future__ import print_function
 import sys
+import time
 import spotipy
 import spotipy.util as util
 from dotenv import load_dotenv
 
 load_dotenv()
 
+def interact(device):
+    loop = True
+    print("Valid Commands: skip, last, pause, play, current, quit")
+    while(loop):
+        cmd = input()
+        try:
+            if(cmd == "skip"):
+                sp.next_track()
+            elif(cmd == "last"):
+                sp.previous_track()
+            elif(cmd == "pause"):
+                sp.pause_playback()
+            elif(cmd == "play"):
+                sp.start_playback()
+            elif(cmd == "current"):
+                current()
+            elif(cmd == "quit"):
+                loop = False
+            else:
+                print("Valid Commands: skip, last, pause, play, current, quit")
+        except spotipy.SpotifyException as err:
+            print(err)
+
+def current():
+    print("Currently Playing: " + sp.currently_playing()['item']['name'] + " - " + sp.currently_playing()['item']['artists'][0]['name'])
 
 scope = 'user-read-playback-state user-modify-playback-state'
 
@@ -16,14 +42,27 @@ else:
     sys.exit()
 
 token = util.prompt_for_user_token(username, scope)
+found = False
 
 if token:
     sp = spotipy.Spotify(auth=token)
-    d = sp.devices()
-    print(d)
-    for i in d['devices']:
-        print(i)
-        if i['is_active']:
-            print(i['name'] + "ACTIVE")
-
-sp.next_track()
+    for i in range(5):
+        d = sp.devices()
+        print("[ATTEMPT "+str(i+1)+"] Searching for active device"),
+        time.sleep(1)
+        print('.'),
+        time.sleep(1)
+        print('.'),
+        time.sleep(1)
+        print('.')
+        for j in d['devices']:
+            if j['is_active']:
+                found = True
+                print(j['name'] + " is active.")
+                time.sleep(0.5)
+                current()
+                time.sleep(0.5)
+                interact(j)
+                break
+        if(found):
+            break
