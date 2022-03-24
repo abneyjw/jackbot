f729213165150efddc33043a9108dcf71c0a84cd
jwabney <jwabney@gmail.com>
2022-03-24 15:30:35 -0400

    Weather timing control added and information displayed better

--- main.py
+++ main.py
@@ -1,28 +1,19 @@
-from tkinter import *
-from tkinter import ttk
 from ctypes import windll
 import time
 from spotify import *
+from weather import *
 windll.shcore.SetProcessDpiAwareness(1)
 
 n = 0
 
 mySpot = Spotify()
 mySpot.setup()
-root = Tk()
-frm = ttk.Frame(root, padding=10)
-frm.grid()
-title= ttk.Label(frm, text="Spotify")
-title.grid(column=0, row=0)
-ttk.Button(frm, text="Prev", command=lambda: mySpot.interact("prev")).grid(column=1, row=1)
-ttk.Button(frm, text="Play/Pause", command=lambda: mySpot.interact("toggle")).grid(column=2, row=1)
-ttk.Button(frm, text="Skip", command=lambda: mySpot.interact("skip")).grid(column=3, row=1)
 
 while True:
-    if (n == 10):
-        title["text"]= mySpot.current()
-        if(not mySpot.playing()):
-            title["text"] = title["text"] + " (paused)"
-        n = 0
-    root.update()
-    n = n + 1
+    time.sleep(1)
+    myText = mySpot.current()
+    if(not mySpot.playing()):
+        myText = myText + " (paused)"
+    n = 0
+    print(myText)
+    
--- weather.py
+++ weather.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 import json
-
+import time
 from accuweather import (
     AccuWeather,
     ApiError,
@@ -11,34 +11,49 @@ from accuweather import (
 )
 from aiohttp import ClientError, ClientSession
 
-LATITUDE = 36.2168
-LONGITUDE = -81.6746
-LOCATION_KEY = "334814"
-API_KEY = "QIYstLjGNnGZxShlvHW4GnhC0aVpZ7z1"
+#This script pulls the latest temperature and conditions from AccuWeather given that more than 30 minutes has passed since the last call.
+#This is because the free version of the AccuWeather API only allows 50 calls a day.
 
-logging.basicConfig(level=logging.DEBUG)
+class Weather:
+    async def main():
+        LATITUDE = 36.2168
+        LONGITUDE = -81.6746
+        LOCATION_KEY = "334814"
+        API_KEY = "QIYstLjGNnGZxShlvHW4GnhC0aVpZ7z1"
+        
+        f = open("currents.txt")
+        content = f.readlines()
+        t = time.localtime()
+        currTime = int(time.strftime("%H",t)) * 60 + int(time.strftime("%M",t))
+        print(int(content[3]) + 30 < currTime)
+        f.close()
+        
+        if(int(content[3]) + 30 < currTime):
+            async with ClientSession() as websession:
+                try:
+                    accuweather = AccuWeather(
+                        API_KEY, websession, latitude=LATITUDE, longitude=LONGITUDE
+                    )
+                    current_conditions = await accuweather.async_get_current_conditions()
+                except (
+                    ApiError,
+                    InvalidApiKeyError,
+                    InvalidCoordinatesError,
+                    ClientError,
+                    RequestsExceededError,
+                ) as error:
+                    print(f"Error: {error}")
+                else:
+                    f = open("currents.txt", "w")
+                    f.write(str(current_conditions['Temperature']['Imperial']['Value']) + "\n")
+                    f.write(str(current_conditions['PrecipitationSummary']['Precipitation']['Imperial']['Value']) + "\n")
+                    f.write(str(current_conditions['WeatherIcon']) + "\n")
+                    t = time.localtime()
+                    f.write(str(int(time.strftime("%H",t)) * 60 + int(time.strftime("%M",t))))
+                    f.close()
+        time.sleep(0.1)
+            
 
-
-async def main():
-    async with ClientSession() as websession:
-        try:
-            accuweather = AccuWeather(
-                API_KEY, websession, latitude=LATITUDE, longitude=LONGITUDE
-            )
-            current_conditions = await accuweather.async_get_current_conditions()
-            forecast = await accuweather.async_get_forecast(metric=True)
-        except (
-            ApiError,
-            InvalidApiKeyError,
-            InvalidCoordinatesError,
-            ClientError,
-            RequestsExceededError,
-        ) as error:
-            print(f"Error: {error}")
-        else:
-            print(current_conditions['Temperature']['Imperial']['Value'])
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-loop.close()
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
+    loop.close()
